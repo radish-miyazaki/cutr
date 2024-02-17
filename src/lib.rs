483f79ff9a8f18fcf437ef5da6400c8c6cb8a9f2
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-17 19:58:21 +0900

    add extract_chars implementation & test

--- src/lib.rs
+++ src/lib.rs
@@ -81,6 +81,30 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
+fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
+    let mut str = String::new();
+    for pos in char_pos {
+        line.chars()
+            .skip(pos.start)
+            .take(pos.end - pos.start)
+            .for_each(|c| str.push(c));
+    }
+
+    str
+}
+
+fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
+    let mut bytes: Vec<u8> = vec![];
+    for pos in byte_pos {
+        line.bytes()
+            .skip(pos.start)
+            .take(pos.end - pos.start)
+            .for_each(|b| bytes.push(b));
+    }
+
+    String::from_utf8_lossy(&bytes).to_string()
+}
+
 pub fn run(cli: Cli) -> MyResult<()> {
     println!("{:?}", cli);
     Ok(())
@@ -208,5 +232,18 @@ mod unit_tests {
         let res = parse_pos("15,19-20");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), vec![14..15, 18..20]);
+    }
+
+    #[test]
+    fn test_extract_chars() {
+        assert_eq!(extract_chars("", &[0..1]), "".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1]), "á".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1, 2..3]), "ác".to_string());
+        assert_eq!(extract_chars("ábc", &[0..3]), "ábc".to_string());
+        assert_eq!(extract_chars("ábc", &[2..3, 1..2]), "cb".to_string());
+        assert_eq!(
+            extract_chars("ábc", &[0..1, 1..2, 4..5]),
+            "áb".to_string()
+        );
     }
 }
